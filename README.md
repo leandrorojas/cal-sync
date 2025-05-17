484047eb37f05d3aa536e6107c50d4851764746d
Leandro Rojas <leandrorojas@gmail.com>
2025-05-17 09:47:08 -0300

    "added base files"

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # cal-sync
-syncs a google calendars into icloud
+sync calendars into icloud
 
 ## requierements
 * PyYAML 6.0.2
+* icalendar 6.2.0
+* pycloud 2.0.0-beta3 (submodule)
--- src/cal_sync.py
+++ src/cal_sync.py
@@ -1,3 +1,7 @@
-#read origin calendars from yaml file
-#get events from google calendar
-#sync with icloud
+# read iCloud calendar from yaml file
+# download iCloud calendar
+# download origin calendars from yaml file
+# read downloaded file
+# iterate through each calendar
+# get events from the current calendar
+# sync with iCloud
