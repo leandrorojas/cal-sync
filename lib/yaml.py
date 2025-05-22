db7c658ea91607adc1abc86478b00dddf4e2406a
Leandro Rojas <leandrorojas@gmail.com>
2025-05-22 19:28:01 -0300

    "added yaml lib and started coding to download calendar events"

---
+++ lib/yaml.py
@@ -0,0 +1,24 @@
+import yaml
+
+class YAMLConfig:
+    __yaml_file = None
+
+    @property
+    def yaml_file(self):
+        global __yaml_file
+        return __yaml_file
+
+    @yaml_file.setter
+    def yaml_file(self, value):
+        global __yaml_file
+        __yaml_file = value
+
+    def read_yaml_setting(self, section:str, key:str):
+        with open(self.yaml_file) as local_yaml_file:
+            yaml_config = yaml.safe_load(local_yaml_file)
+
+            try:
+                return yaml_config[section][key]
+            except Exception as err:
+                print(f"Section: {section}, Key: {key} not found in YAML file. Error reading YAML file ({self.yaml_file}): {err}")
+                return None
--- src/cal_sync.py
+++ src/cal_sync.py
@@ -1,7 +1,40 @@
-# read iCloud calendar from yaml file
-# download iCloud calendar
-# download origin calendars from yaml file
+# ✅ read iCloud calendar from yaml file
+# ✅ download iCloud calendar
+# filter events from iCloud calendar by tag
+# download destination calendars from yaml file
 # read downloaded file
 # iterate through each calendar
 # get events from the current calendar
 # sync with iCloud
+
+from pyicloud.pyicloud import PyiCloudService
+from lib.yaml import YAMLConfig
+from datetime import datetime, timedelta
+
+cal_sync_config = YAMLConfig()
+cal_sync_config.yaml_file = "cal_sync.yaml"
+
+icloud_service = PyiCloudService(apple_id=cal_sync_config.read_yaml_setting("icloud-calendar", "user"), password=cal_sync_config.read_yaml_setting("icloud-calendar", "pass"))
+
+if icloud_service.requires_2fa:
+    code = input("Enter the security code: ")
+    result = icloud_service.validate_2fa_code(code)
+
+    if not result:
+        print("Failed to verify security code")
+
+    if not icloud_service.is_trusted_session:
+        print("Session is not trusted. Requesting trust...")
+        result = icloud_service.trust_session()
+        print("Session trust result %s" % result)
+
+        if not result:
+            print("Failed to request trust. You will likely be prompted for the code again in the coming weeks")
+
+tmp_date = datetime.now().strftime("%Y-%m-%d")
+# get events just for tomorrow
+events_from = datetime.strptime(tmp_date, "%Y-%m-%d") + timedelta(days=1)
+events_to = datetime.strptime(tmp_date, "%Y-%m-%d") + timedelta(days=1, hours=23, minutes=59, seconds=59)
+
+with open("/Users/lrojas/Downloads/icloud_calendar.json", "w+") as ics_file:
+    ics_file.write(str(icloud_service.calendar.get_events(from_dt=events_from, to_dt=events_to)))
