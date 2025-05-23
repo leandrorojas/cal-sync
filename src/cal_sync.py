198a54dfb38486556909e5fd56eede9d49a72f6a
Leandro Rojas <leandrorojas@gmail.com>
2025-05-22 21:21:48 -0300

    "initial commit"

--- src/cal_sync.py
+++ src/cal_sync.py
@@ -7,6 +7,9 @@
 # get events from the current calendar
 # sync with iCloud
 
+import icalendar
+import urllib.request
+
 from pyicloud.pyicloud import PyiCloudService
 from lib.yaml import YAMLConfig
 from datetime import datetime, timedelta
@@ -19,6 +22,7 @@ icloud_service = PyiCloudService(apple_id=cal_sync_config.read_yaml_setting("icl
 if icloud_service.requires_2fa:
     code = input("Enter the security code: ")
     result = icloud_service.validate_2fa_code(code)
+    code = None
 
     if not result:
         print("Failed to verify security code")
@@ -31,10 +35,12 @@ if icloud_service.requires_2fa:
         if not result:
             print("Failed to request trust. You will likely be prompted for the code again in the coming weeks")
 
-tmp_date = datetime.now().strftime("%Y-%m-%d")
+tomorrow = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
 # get events just for tomorrow
-events_from = datetime.strptime(tmp_date, "%Y-%m-%d") + timedelta(days=1)
-events_to = datetime.strptime(tmp_date, "%Y-%m-%d") + timedelta(days=1, hours=23, minutes=59, seconds=59)
+events_from = datetime.strptime(tomorrow, "%Y-%m-%d")
+events_to = datetime.strptime(tomorrow, "%Y-%m-%d") + timedelta(hours=23, minutes=59, seconds=59)
 
-with open("/Users/lrojas/Downloads/icloud_calendar.json", "w+") as ics_file:
-    ics_file.write(str(icloud_service.calendar.get_events(from_dt=events_from, to_dt=events_to)))
+tomorrow_events = icloud_service.calendar.get_events(from_dt=events_from, to_dt=events_to)
+
+fileICS = open(icsFile, "rb")
+icalendar.Calendar.from_ical(fileICS.read())
